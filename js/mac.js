8ad50f4d55c2402542c0932ea956953c6d3320ac
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-08-26 17:59:15 +0600

    last commit

--- js/mac.js
+++ js/mac.js
@@ -1,40 +1,44 @@
-// function for add extra cost
-function addExtraCost(btnId, textId, price, getTotalCost ){
+// for single total cost
+function singleTotalCost(textId){
+    const extraCost = document.getElementById(textId);
+    const extraCostText = extraCost.innerText;
+    const extraCostTotal = parseInt(extraCostText);
+    return extraCostTotal;
+}
+
+
+// function for add extra cost and total cost
+function addExtraCost(btnId, textId, price,){
     document.getElementById(btnId).addEventListener('click', function(){
         const extraPrice = document.getElementById(textId);
         extraPrice.innerText = price;
-        getTotalCost()
-        return extraPrice;
+        const memoryTotalCost = singleTotalCost('extra-memory');
+        const storageTotalCost = singleTotalCost('extra-storage');
+        const deliveryTotalCost = singleTotalCost('delivery-Cost');
+        const totalCost = document.getElementById('total-cost');
+        totalCost.innerText = memoryTotalCost + storageTotalCost + 1299 + deliveryTotalCost;
+        const subTotal = document.getElementById('sub-total');
+        subTotal.innerText = totalCost.innerText;
+
     })
 }
 // add memory cost 
-addExtraCost('eight-btn', 'extra-memory', 0,getTotalCost);
-addExtraCost('Sixteen-btn', 'extra-memory', 180);
+addExtraCost('eight-btn', 'extra-memory', 0,);
+addExtraCost('Sixteen-btn', 'extra-memory', 180,);
+
+
 // add storage cost
-addExtraCost('256-btn', 'extra-storage' ,0);
+addExtraCost('256-btn', 'extra-storage' ,0,);
 addExtraCost('512-btn', 'extra-storage' ,100);
 addExtraCost('1-btn', 'extra-storage' ,180);
+
+
 // add delivery cost
 addExtraCost('free-btn', 'delivery-Cost' ,0);
 addExtraCost('express-btn', 'delivery-Cost' ,20); 
-// function for  total price  
-function getTotalCost(){
-    const memoryTotal = document.getElementById('extra-memory');
-    const memoryText = memoryTotal.innerText;
-    const memoryTotalCost = parseInt(memoryText);
-    const storageTotal = document.getElementById('extra-storage');
-    const storageText = storageTotal.innerText;
-    const storageTotalCost = parseInt(storageText);
-    const deliveryTotal = document.getElementById('Delivery-Cost');
-    const deliveryCost = deliveryTotal.innerText;
-    const deliveryTotalCost = parseInt(deliveryCost);
-    const totalCost = document.getElementById('total-cost');
-    totalCost.innerText = memoryTotalCost + storageTotalCost + 1299 + deliveryTotalCost;
-    const subTotal = document.getElementById('sub-total');
-    subTotal.innerText = totalCost.innerText;    
-}
 
-// promocode code section
+
+//section promocode
 document.getElementById('promo-btn').addEventListener('click', function(){
     const promoField = document.getElementById('promo-field');
     const promoCode = promoField.value;
